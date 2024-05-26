713314a3458a7d9b3e49fff201d7905341e604fc
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-26 18:08:09 -0500

    Cambios en la pagina principal del blog, donde contiene la barra de busqueda y el boton de iniciar sesion direcciona al contenido. PriscilaZ

--- Views/InicioSesion.xaml.cs
+++ Views/InicioSesion.xaml.cs
@@ -6,4 +6,9 @@ public partial class InicioSesion : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ClickIngreso(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new PaginaPrincipal());
+    }
 }
--- Views/PaginaPrincipal.xaml.cs
+++ Views/PaginaPrincipal.xaml.cs
@@ -1,9 +1,39 @@
+using BLOGSOCIALUDLA.Models;
+using System.Collections.ObjectModel;
+
 namespace BLOGSOCIALUDLA.Views;
 
 public partial class PaginaPrincipal : ContentPage
 {
-	public PaginaPrincipal()
+
+    public ObservableCollection<Facultad> Facultades { get; set; }
+    //Copia de la coleccion para mostrar las imagenes en un orden diferente
+    public ObservableCollection<Facultad> Facultades2 { get; set; }
+    public PaginaPrincipal()
 	{
 		InitializeComponent();
-	}
+        InitializeFacultades();
+        BindingContext = this;
+    }
+
+    private void InitializeFacultades()
+    {
+        Facultades = new ObservableCollection<Facultad>
+        {
+            new Facultad{Name= "FICA", ReadTime= new TimeSpan(0,30,0), Image= "fica.jpg"},
+            new Facultad{Name= "Facultad de Medicina", ReadTime= new TimeSpan(0,30,0), Image= "medicina.jpg"},
+            new Facultad{Name= "Facultad de Derecho", ReadTime= new TimeSpan(0,30,0), Image= "derecho.jpg"},
+            new Facultad{Name= "Facultad de Arquitectura", ReadTime= new TimeSpan(0,30,0), Image= "arquitectura.jpg"},
+            new Facultad{Name= "NODO", ReadTime= new TimeSpan(0,30,0), Image= "nodo.jpg"},
+            new Facultad{Name= "Escuela de Música", ReadTime= new TimeSpan(0,30,0), Image= "musica.jpg"}
+        };
+        Facultades2 = new ObservableCollection<Facultad>
+        {
+            new Facultad{Name= "Clínica de Odontología", ReadTime= new TimeSpan(0,30,0), Image= "odontologia.jpg"},
+            new Facultad{Name= "Escuela de Gastronomía", ReadTime= new TimeSpan(0,30,0), Image= "gastronomia.jpg"},
+            new Facultad{Name= "Servicios de Psicología", ReadTime= new TimeSpan(0,30,0), Image= "psicologia.jpg"},
+            new Facultad{Name= "Biblioteca", ReadTime= new TimeSpan(0,30,0), Image= "economia.jpg"},
+
+        };
+    }
 }
