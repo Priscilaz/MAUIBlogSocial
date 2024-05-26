80aebce9bcd1996a70368d586cae1a33955a6a00
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-26 18:55:57 -0500

    Cambios en el diseño de la interfaz. PriscilaZ

--- Views/PaginaPrincipal.xaml.cs
+++ Views/PaginaPrincipal.xaml.cs
@@ -20,12 +20,12 @@ public partial class PaginaPrincipal : ContentPage
     {
         Facultades = new ObservableCollection<Facultad>
         {
-            new Facultad{Name= "FICA", ReadTime= new TimeSpan(0,30,0), Image= "fica.jpg"},
-            new Facultad{Name= "Facultad de Medicina", ReadTime= new TimeSpan(0,30,0), Image= "medicina.jpg"},
-            new Facultad{Name= "Facultad de Derecho", ReadTime= new TimeSpan(0,30,0), Image= "derecho.jpg"},
-            new Facultad{Name= "Facultad de Arquitectura", ReadTime= new TimeSpan(0,30,0), Image= "arquitectura.jpg"},
-            new Facultad{Name= "NODO", ReadTime= new TimeSpan(0,30,0), Image= "nodo.jpg"},
-            new Facultad{Name= "Escuela de Música", ReadTime= new TimeSpan(0,30,0), Image= "musica.jpg"}
+            new Facultad{Name= "FICA", ReadTime= new TimeSpan(0,4,0), Image= "fica.jpg"},
+            new Facultad{Name= "Facultad de Medicina", ReadTime= new TimeSpan(0,3,0), Image= "medicina.jpg"},
+            new Facultad{Name= "Facultad de Derecho", ReadTime= new TimeSpan(0,2,0), Image= "derecho.jpg"},
+            new Facultad{Name= "Facultad de Arquitectura", ReadTime= new TimeSpan(0,3,0), Image= "arquitectura.jpg"},
+            new Facultad{Name= "NODO", ReadTime= new TimeSpan(0,3,0), Image= "nodo.jpg"},
+            new Facultad{Name= "Escuela de Música", ReadTime= new TimeSpan(0,2,0), Image= "musica.jpg"}
         };
         Facultades2 = new ObservableCollection<Facultad>
         {
