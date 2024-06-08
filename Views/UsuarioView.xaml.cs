d35dc19ce3bd800971335226d34168e23bb096af
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-07 21:43:09 -0500

    Diseño de Perfil de Usuario

--- Views/UsuarioView.xaml.cs
+++ Views/UsuarioView.xaml.cs
@@ -1,9 +1,35 @@
-namespace BLOGSOCIALUDLA.Views;
+using BLOGSOCIALUDLA.Models;
+using BLOGSOCIALUDLA.Models.BLOGSOCIALUDLA.Models;
+using Microsoft.Maui.Controls;
+using System.Linq;
 
-public partial class UsuarioView : ContentPage
+namespace BLOGSOCIALUDLA.Views
 {
-	public UsuarioView()
-	{
-		InitializeComponent();
-	}
+    public partial class UsuarioView : ContentPage
+    {
+        private User _currentUser;
+
+        public UsuarioView(string username)
+        {
+            InitializeComponent();
+            DatosDelUsuario(username);
+        }
+
+        private void DatosDelUsuario(string username)
+        {
+            _currentUser = UserData.Users.FirstOrDefault(user => user.Username == username);
+
+            if (_currentUser != null)
+            {
+                NombresLabel.Text = _currentUser.Nombres;
+                ApellidosLabel.Text = _currentUser.Apellidos;
+                NumeroTelefonoLabel.Text = _currentUser.NumeroTelefono;
+                CorreoLabel.Text = _currentUser.Username;
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario no encontrado.", "OK");
+            }
+        }
+    }
 }
