518c84acf09921c1ad3049f5f0fe1bf1be154a9f
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-09 13:36:30 -0500

    Cambio PostNodo

--- Views/PostNodo.xaml.cs
+++ Views/PostNodo.xaml.cs
@@ -9,76 +9,39 @@ using BLOGSOCIALUDLA.Models;
 namespace BLOGSOCIALUDLA.Views
 {
     public partial class PostNodo : ContentPage
+
     {
-        private ObservableCollection<Comentario> comentarios;
+        public ObservableCollection<Post> Posts { get; set; }
 
         public PostNodo()
         {
             InitializeComponent();
-            comentarios = new ObservableCollection<Comentario>();
-            listaComentarios.ItemsSource = comentarios;
+            Posts = new ObservableCollection<Post>(DataPost.Posts);
+            BindingContext = this;
         }
 
-        protected override async void OnAppearing()
+        private async void PostPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            base.OnAppearing();
-            await LoadComentarios();
-        }
-
-        //metodo que nos enseñó nuestro pana
-        private async Task LoadComentarios()
-        {
-            List<Comentario> ListaComentarios = await DataComentario.GetComentarios();
-            foreach (var comentario in ListaComentarios)
+            var selectedPost = (sender as Picker)?.SelectedItem as Post;
+            if (selectedPost != null)
             {
-                comentarios.Add(comentario);
+                await Navigation.PushAsync(new PostSeleccionado(selectedPost));
             }
         }
 
-        private void OnClickShowDetails(object sender, SelectedItemChangedEventArgs e)
+        private async void ClickPostFica(object sender, EventArgs e)
         {
-            if (e.SelectedItem == null)
-                return;
-
-            var selectedComment = e.SelectedItem as Comentario;
-
-            DisplayAlert("Comentario seleccionado", $"Comentario: {selectedComment.Contenido}", "OK");
-
-            listaComentarios.SelectedItem = null;
+            var nuevaPage = new AddPostPage();
+            nuevaPage.PostAgregado += NuevaPage_PostAgregado;
+            await Navigation.PushAsync(nuevaPage);
         }
 
-        private void ClickComentarioNodo(object sender, EventArgs e)
+        private void NuevaPage_PostAgregado(object sender, Post e)
         {
-            string comentarioTexto = comentarioEntry.Text;
-
-            if (string.IsNullOrWhiteSpace(comentarioTexto))
-            {
-                DisplayAlert("Error", "El comentario no puede estar vacío.", "OK");
-                return;
-            }
-
-            var nuevoComentario = new Comentario
-            {
-                Contenido = comentarioTexto
-            };
-
-            comentarios.Add(nuevoComentario);
-
-            DisplayAlert("Comentario añadido", "Tu comentario ha sido añadido exitosamente.", "OK");
-
-            comentarioEntry.Text = string.Empty;
-            
-        }
-
-        private void ClickPostNodo(object sender, EventArgs e)
-        {
-            DisplayAlert("Post añadido", "Tu post ha sido añadido exitosamente.", "OK");
-
-        }
-
-        private async void Volver(object sender, EventArgs e)
-        {
-            await Navigation.PopAsync();
+            DataPost.AgregarPost(e);
+            Posts.Add(e);
         }
     }
 }
+    
+
